dea86c71f726d0c7911f112ca30a47e73cccf484
Anis1only <90601348+Anis1only@users.noreply.github.com>
2025-03-13 13:16:13 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+public class App {
+    // Constructor
+    public App() {}
+
+    // Method to return a message
+    public String getMessage() {
+        return "Hello, World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
